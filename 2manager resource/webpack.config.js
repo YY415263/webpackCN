c5d8588096eda1b328aeb5795c2d5e75d200d9bd
YY415263 <1830924456@qq.com>
2018-02-06 17:40:49 +0800

    manager resouce

--- 2manager resource/webpack.config.js
+++ 2manager resource/webpack.config.js
@@ -8,5 +8,40 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+         rules: [
+          {//样式
+             test: /\.css$/,
+             use: [
+               'style-loader',
+               'css-loader'
+             ]
+          },//图片
+          {
+              test: /\.(png|svg|jpg|gif)$/,
+              use: [
+              'file-loader'
+              ]
+          },//字体
+           {
+               test: /\.(woff|woff2|eot|ttf|otf)$/,
+               use: [
+               'file-loader'
+               ]
+           },//数据
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+               'csv-loader'
+                ]
+            },//数据
+             {
+                 test: /\.xml$/,
+                 use: [
+                'xml-loader'
+                ]
+            }
+     ]
+   }
 };
