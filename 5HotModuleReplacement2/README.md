09036b2d9f77f599bde683a30a1f803994db14f2
YY415263 <1830924456@qq.com>
2018-02-07 14:36:04 +0800

    HotModuleReplacement2

---
+++ 5HotModuleReplacement2/README.md
@@ -0,0 +1,4 @@
+
+1,如果你使用了 webpack-dev-middleware 而没有使用 webpack-dev-server，
+请使用 webpack-hot-middleware package 包，以在你的自定义服务或应用程序上启用 HMR。
+
---
+++ 5HotModuleReplacement2/src/print.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zhekexinxi on 07/02/2018.
+ */
+export default function printMe() {
+    alert('Updating print.js...');
+}
