819ae2a8eddeab4c5ebf15d30abffffdf376e164
YY415263 <1830924456@qq.com>
2018-02-07 14:37:56 +0800

    HotModuleReplacement2

--- 4develop/src/index.js
+++ 4develop/src/index.js
@@ -6,10 +6,8 @@ import printMe from './print';
 
 function component() {
     var element = document.createElement('div');
-
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
 
     var btn = document.createElement('button');
     btn.innerHTML = 'click me and check the console';
--- 5HotModuleReplacement/src/print.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zhekexinxi on 07/02/2018.
- */
-export default function printMe() {
-    alert('Updating print.js...');
-}
