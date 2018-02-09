b559976f98c3bae9633cc3870703de9509bfccd9
YY415263 <1830924456@qq.com>
2018-02-09 10:36:29 +0800

    HotModuleReplacement2

--- 6treeShaking/webpack.config.js
+++ 6treeShaking/webpack.config.js
@@ -8,7 +8,6 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
     entry: {
-        // app: './src/index.js',
         app:[
             'webpack-hot-middleware/client',
             './src/index.js'
@@ -26,8 +25,9 @@ module.exports = {
         ]
     },
     output: {
-        filename: '[name].bundle.js',//打包js的名字
-        publicPath: '/' //打包成功后的文件路径 ,以确保文件资源能够在 http://localhost:3000 下正确访问 (http://localhost:3000/index.html)
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),//打包js的名字
+        publicPath: '/'
     },
     plugins:[
         new HtmlWebpackPlugin({
